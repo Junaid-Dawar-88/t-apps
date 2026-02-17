af72bcb98d46d35a9ff0134744d5477236607f11
Junaid-Dawar-88 <junaidiqbal@gmail.com>
2026-02-17 16:34:24 +0500

    up

--- app/actions/student-actions.ts
+++ app/actions/student-actions.ts
@@ -5,27 +5,27 @@ import prisma from "@/lib/prisma";
 // Create a new student
 // ------------------------
 export async function addStudent(formData: FormData) {
-  const name = formData.get("name")?.toString();
-  const father = formData.get("father")?.toString();
-  const roll_number = formData.get("roll_number")?.toString();
-  const phone = formData.get("phone")?.toString();
-  const address = formData.get("address")?.toString();
+  const name = formData.get("name")?.toString() || "";
+  const father = formData.get("father")?.toString() || "";
+  const roll_number = formData.get("roll_number")?.toString() || "";
+  const phone = formData.get("phone")?.toString() || "";    
+  const address = formData.get("address")?.toString() || "";
   const classId = parseInt(formData.get("class_id")?.toString() || "");
 
   if (!name || !father || !roll_number || !classId) {
-    return 
+    return;
   }
-  
+
   const classRecord = await prisma.class.findUnique({ where: { id: classId } });
   if (!classRecord) throw new Error("Selected class not found");
-   
+
   const student = await prisma.student.create({
     data: {
       name,
       father,
       roll_number,
-      phone: phone ?? "",
-      address: address ?? '',
+      phone,  
+      address, 
       class: { connect: { id: classId } },
     },
   });
